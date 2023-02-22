19e053ec6c57bba43a94d8f072c05834d9dd9f81
VirajKadia <viraj.kadia@ishitva.in>
2023-02-22 11:26:37 +0530

    added cout

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "mainwindow.H"
+#include <iostream>
 
 #include <QApplication>
 
@@ -6,6 +7,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    std::cout<<"123";
     w.show();
     return a.exec();
 }
